c96e8de21c4320c2623a2a7b016915a652656b62
Jonathan Fuentes <john.fuen95@gmail.com>
2018-09-28 02:45:13 -0500

    Add files via upload

--- todos.js
+++ todos.js
@@ -1,11 +1,22 @@
 //Check off specific todos by clicking
-$('li').click(function(){
+$("ul").on("click", "li", function(){
   $(this).toggleClass('completed');
 });
 
-$('span').click(function(e){
-  $(this).parent().fadeOut(1000, function(){
+//Click on X to delete Todo
+$('ul').on('click','span',function(e){
+  $(this).parent().fadeOut(500, function(){
     $(this).remove();
   });
   e.stopPropagation();
 });
+
+  $("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+      //Grabbing new todo text from input
+      var todoText = $(this).val();
+      $(this).val("");
+      //Create a new li and add to ul
+      $("ul").append('<li><span><i class="far fa-trash-alt"></i></span> ' + todoText + '</li>');
+    }
+  });
